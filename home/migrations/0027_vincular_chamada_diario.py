55a25dd2502699938e97b7805da1f5c53258e66f
goutemberg <gouberg@gmail.com>
2026-02-23 15:36:48 -0300

    ajuste-0027

--- home/migrations/0027_vincular_chamada_diario.py
+++ home/migrations/0027_vincular_chamada_diario.py
@@ -1,36 +1,40 @@
-from django.db import migrations
+from django.db import migrations, connection
 
 
 def vincular_chamadas_a_diarios(apps, schema_editor):
     Chamada = apps.get_model("home", "Chamada")
     DiarioDeClasse = apps.get_model("home", "DiarioDeClasse")
 
-    for chamada in Chamada.objects.all().iterator():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT id, data, turma_id, disciplina_id, professor_id
+            FROM home_chamada
+            WHERE diario_id IS NULL
+        """)
+        chamadas = cursor.fetchall()
 
-        if chamada.diario_id is not None:
-            continue
+    for chamada_id, data, turma_id, disciplina_id, professor_id in chamadas:
 
         diario_qs = DiarioDeClasse.objects.filter(
-            data_ministrada=chamada.data,
-            turma=chamada.turma,
-            disciplina=chamada.disciplina,
+            data_ministrada=data,
+            turma_id=turma_id,
+            disciplina_id=disciplina_id,
         ).order_by("hora_inicio")
 
         diario = diario_qs.first()
 
         if not diario:
             diario = DiarioDeClasse.objects.create(
-                data_ministrada=chamada.data,
-                turma=chamada.turma,
-                disciplina=chamada.disciplina,
-                professor=chamada.professor,
-                escola=chamada.turma.escola,
+                data_ministrada=data,
+                turma_id=turma_id,
+                disciplina_id=disciplina_id,
+                professor_id=professor_id,
+                escola_id=DiarioDeClasse.objects.model._meta.get_field("escola").remote_field.model.objects.filter(turma_id=turma_id).values_list("escola_id", flat=True).first(),
                 status="REALIZADA",
                 resumo_conteudo="Diário gerado automaticamente (migração)",
             )
 
-        chamada.diario = diario
-        chamada.save(update_fields=["diario"])
+        Chamada.objects.filter(id=chamada_id).update(diario=diario)
 
 
 class Migration(migrations.Migration):
