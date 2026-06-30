de7412e7680ca0b5fbfe0f1a4d293eb74568078d
goutemberg <gouberg@gmail.com>
2026-06-30 21:02:10 +0200

    ajuste-na-nota

--- api/urls_chamada.py
+++ api/urls_chamada.py
@@ -1,8 +1,33 @@
 from django.urls import path
-from .views.chamada import salvar_chamada_api, consultar_chamada_api, atualizar_chamada_api 
+from .views.chamada import (
+    salvar_chamada_api,
+    consultar_chamada_api,
+    atualizar_chamada_api,
+    datas_chamada_api,
+)
 
 urlpatterns = [
-    path('chamada/salvar/', salvar_chamada_api, name='salvar_chamada_api'),
-    path('chamada/consultar/', consultar_chamada_api, name='consultar_chamada_api'),
-    path('chamada/atualizar/<int:diario_id>/', atualizar_chamada_api, name='atualizar_chamada_api'),
+    path(
+        'chamada/salvar/',
+        salvar_chamada_api,
+        name='salvar_chamada_api'
+    ),
+
+    path(
+        'chamada/consultar/',
+        consultar_chamada_api,
+        name='consultar_chamada_api'
+    ),
+
+    path(
+        'chamada/atualizar/<int:diario_id>/',
+        atualizar_chamada_api,
+        name='atualizar_chamada_api'
+    ),
+
+    path(
+    'datas_chamada/',
+    datas_chamada_api,
+    name='datas_chamada_api'
+),
 ]
