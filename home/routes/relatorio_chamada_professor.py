825e32be99ea38d39f537213d6de1d2ca77fd80a
goutemberg <gouberg@gmail.com>
2026-07-07 23:54:38 +0200

    gerar-pdf

--- home/routes/relatorio_chamada_professor.py
+++ home/routes/relatorio_chamada_professor.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from home.views.relatorio_chamada_professor import relatorio_chamada_professor
+from home.views.relatorio_chamada_professor import (
+    relatorio_chamada_professor,
+    relatorio_chamada_professor_pdf,
+)
 
 app_name = "relatorio_chamada_professor"
 
@@ -9,5 +12,10 @@ urlpatterns = [
         "relatorio-chamada-professor/",
         relatorio_chamada_professor,
         name="relatorio_chamada_professor",
+    ),
+    path(
+        "relatorio-chamada-professor/pdf/",
+        relatorio_chamada_professor_pdf,
+        name="relatorio_chamada_professor_pdf",
     ),
 ]
